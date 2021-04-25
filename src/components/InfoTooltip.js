4da35147e56c2e03e5fca36bb2296acd7baac224
Kaitlyn Hiller <k.m.hiller@gmail.com>
2021-04-25 19:05:34 -0400

    register modal designed

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,18 +1,20 @@
 //a modal window component that informs the user whether they've been registered successfully
 
 import React from 'react';
-import Success from "../images/success.png";
+import success from "../images/success.png";
+import fail from "../images/fail.png";
 
 function InfoTooltip(props) {
   
-  
+  const textSuccess = "Success! You have now been registered."
+  const textFail = "Oops, something went wrong! Please try again."
 
   return(
     <div className={`popup ${props.isOpen ? 'popup_open' : ""}`}>
-      <div className="popup__container">
+      <div className="popup__container popup__container_modal">
         <button className="popup__close-button" aria-label="Close pop up" type="button" onClick={props.onClose}></button>
-        <img src='Success'></img>
-        {props.children}
+        <img className="popup__modal-image" src={success}></img>
+        <p className="popup__modal-text">{textSuccess}</p>
       </div>
     </div>
   )
